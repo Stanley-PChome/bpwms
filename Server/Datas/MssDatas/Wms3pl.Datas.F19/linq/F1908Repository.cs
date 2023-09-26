9fc52e9454dad0c67d4330ae2c1369a6f05726d6
albert_lin <albert_lin@ALBERTNB>
2023-09-26 18:18:15 +0800

    2347 - 修正廠商主檔維護查詢會報錯

--- Server/Datas/MssDatas/Wms3pl.Datas.F19/linq/F1908Repository.cs
+++ Server/Datas/MssDatas/Wms3pl.Datas.F19/linq/F1908Repository.cs
@@ -25,35 +25,6 @@ namespace Wms3pl.Datas.F19
                         .FirstOrDefault();
 		}
 
-		/// <summary>
-		/// 取得已過濾人員權限的廠商主檔
-		/// </summary>
-		/// <param name="gupCode"></param>
-		/// <param name="custCode"></param>
-		/// <param name="vnrCode"></param>
-		/// <param name="vnrName"></param>
-		/// <returns></returns>
-		public IQueryable<F1908> GetAllowedF1908s(string gupCode, string vnrCode, string vnrName, string custCode)
-		{
-            //命名參考原始碼
-            var query = _db.F1908s
-                        .Join(_db.F192402s, a => a.GUP_CODE, j => j.GUP_CODE, (a, j) => new { a, j })
-                        .Join(_db.F1909s, aj => aj.a.GUP_CODE, b => b.GUP_CODE, (aj, b) => new { aj, b })
-                        .Where(x => x.b.CUST_CODE == custCode);
-            query = query
-                        .Where(x => x.aj.a.STATUS != "9")
-                        .Where(x => x.aj.a.GUP_CODE == (string.IsNullOrEmpty(gupCode) ? x.aj.a.GUP_CODE : gupCode))
-                        .Where(x => x.aj.a.VNR_CODE == (string.IsNullOrEmpty(vnrCode) ? x.aj.a.VNR_CODE : vnrCode))
-                        .Where(x => x.aj.j.EMP_ID != Current.Staff)
-                        .Where(x => x.aj.a.CUST_CODE == ((x.b.ALLOWGUP_VNRSHARE == "1") ? "0" : custCode));
-            if (!string.IsNullOrEmpty(vnrName))
-                query = query.Where(x => x.aj.a.VNR_NAME.Contains(vnrName));
-
-            //原始碼: -- 用 DISTINCT 是因人員權限包含 DC_CODE 可能會重複
-            return query
-                    .Select(x => x.aj.a).Distinct();
-		}
-
 		public F1908 GetEnabledVnrData(string gupCode,string custCode,string vnrCode)
 		{
             return _db.F1908s
