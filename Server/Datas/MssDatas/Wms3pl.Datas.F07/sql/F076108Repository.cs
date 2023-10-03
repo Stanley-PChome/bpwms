b9c73404d7580ca471c79e1c9e39c07945dcdaea
Neo Chen <neochen0401@gmail.com>
2023-10-03 15:30:54 +0800

    no2351新增F076108 Table

---
+++ Server/Datas/MssDatas/Wms3pl.Datas.F07/sql/F076108Repository.cs
@@ -0,0 +1,9 @@
+﻿using Wms3pl.DBCore;
+using Wms3pl.WebServices.DataCommon;
+
+namespace Wms3pl.Datas.F07
+{
+	public partial class F076108Repository : RepositoryBase<F076108, Wms3plDbContext, F076108Repository>
+	{
+	}
+}
