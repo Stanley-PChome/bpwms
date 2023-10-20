1bc684adf76e7453988b3a51af4e3f5444d6daad
albert_lin <albert_lin@ALBERTNB>
2023-10-20 10:56:54 +0800

    (DEV)PHWMS_GCP_v3.0.24.0

--- Client/WpfClient/Properties/AssemblyInfo.cs
+++ Client/WpfClient/Properties/AssemblyInfo.cs
@@ -59,7 +59,7 @@ using System.Windows.Markup;
 #elif (Ph_A7)
 [assembly: AssemblyVersion("6.0.44.0")]
 #elif PhTest
-[assembly: AssemblyVersion("3.0.23.0")]
+[assembly: AssemblyVersion("3.0.24.0")]
 #elif PhTest_A7
 [assembly: AssemblyVersion("7.0.4.0")]
 #endif
